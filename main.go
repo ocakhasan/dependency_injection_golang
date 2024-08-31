ffe53dadccc9042a67c42560dbd0e5f6f154fab1
Hasan Ocak <hasanocak.tech@gmail.com>
2024-08-31 18:53:35 +0300

    fix the code

--- main.go
+++ main.go
@@ -7,5 +7,10 @@ import (
 func main() {
 	injector := Injector()
 
-	injector.
+	// Resolve the OrderService and use it
+	orderService := do.MustInvoke[*OrderService](injector)
+	err := orderService.ProcessOrder("12345", 100.00)
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Dependency Injection Example
+
+This project is an example to show the dependency injection via [samber/do](https://github.com/samber/do)
